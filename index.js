37f72661ef45c71715fe944c75557a40ed4d04ee
deveshbis <deveshbiswas.cse@gmail.com>
2024-05-12 15:25:39 +0600

    use delete method to request food manage

--- index.js
+++ index.js
@@ -217,6 +217,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/deleteData/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
