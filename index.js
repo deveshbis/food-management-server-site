5eeb5c5888d84f04aaa8ce21adbbec6dc08b7f0d
deveshbis <deveshbiswas.cse@gmail.com>
2024-12-03 18:04:52 +0600

    updated server

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.Port || 5000;
 
-
 const app = express()
 
 // middleware
-// app.use(cors());
-// app.use(express.json());
 const corsOptions = {
   origin: [
     'http://localhost:5173',
@@ -56,14 +53,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-
-
-
 
     //jwt
-
     app.post('/jwt', async (req, res) => {
       const user = req.body
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
@@ -90,26 +81,13 @@ async function run() {
     })
 
 
-
-
-
-
-//featuredFoods
-
-
+    //featuredFoods
     const featuredFoodsCollection = client.db('fooddb').collection('featuredFoods')
 
     app.get('/featuredFoods', async (req, res) => {
       const result = await featuredFoodsCollection.find().toArray()
       res.send(result)
     })
-
-    // app.get('/food/:id', async (req, res) => {
-    //   const id = req.params.id
-    //   const query = { _id: new ObjectId(id) }
-    //   const result = await featuredFoodsCollection.findOne(query)
-    //   res.send(result)
-    // })
 
     app.get('/singleDetails/:id', async (req, res) => {
       const id = req.params.id;
@@ -119,20 +97,11 @@ async function run() {
     })
 
 
-
-
-
-
-
-
-
-
     //user Data
-
     const userCollection = client.db('fooddb').collection('userData');
 
     app.get('/userData', async (req, res) => {
-      const cursor = reqCollection.find();
+      const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
@@ -201,11 +170,6 @@ async function run() {
       res.send(result);
     })
 
-
-
-
-
-
     // Get all jobs data from db for sorting and search
     app.get('/userData', async (req, res) => {
       const sort = req.query.sort
@@ -219,26 +183,7 @@ async function run() {
 
       res.send(result)
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
     //request Data
     const reqCollection = client.db('fooddb').collection('reqData');
@@ -252,7 +197,7 @@ async function run() {
 
     app.get('/reqData/:email', verifyToken, async (req, res) => {
       const email = req.params.email
-      const query = { 'postOwner.email': email }
+      const query = { email: email }
       const result = await reqCollection.find(query).toArray()
       res.send(result)
     })
@@ -269,9 +214,6 @@ async function run() {
       const result = await userCollection.deleteOne(query);
       res.send(result);
     })
-
-
-
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
